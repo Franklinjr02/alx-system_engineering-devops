59ed31e4231f5f20d1808e78700672770368d005
franklinjr02 <franklinmuoneke02@gmail.com>
2022-10-20 03:16:01 +0000

    2nd task

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,7 @@
-# Create a file
-file { '/tmp/school':
-  ensure  => file,
-  path    => '/tmp/school',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet'
-}
+# Creating A file in tmp folder named holberton
+file { '/tmp/holberton':
+    ensure  => 'present',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet', }
